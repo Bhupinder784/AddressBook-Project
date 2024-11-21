a362d757b86d9915d1d3e916310323cb5495bf47
bhupinder <bhupindernh784@gmail.com>
2024-11-21 17:05:58 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/addressbook/AddressBookingApplication.java
@@ -0,0 +1,14 @@
+package com.addressbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookingApplication.class, args);
+		System.out.println("Hello World!!");
+	}
+
+}
