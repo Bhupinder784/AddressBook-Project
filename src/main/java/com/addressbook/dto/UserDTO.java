dc1993994f04cd231b40f09d949cc29d4948e8de
bhupinder <bhupindernh784@gmail.com>
2024-11-21 17:50:09 +0530

    user database setup

---
+++ src/main/java/com/addressbook/dto/UserDTO.java
@@ -0,0 +1,16 @@
+package com.addressbook.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private String id;
+    private String name;
+    private String email;
+    private String role;
+}
+
+
+
+
+//UserDTO (for API communication)
---
+++ src/main/java/com/addressbook/model/User.java
@@ -0,0 +1,38 @@
+package com.addressbook.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.USER;
+
+    @CreationTimestamp
+    private LocalDateTime createAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    public enum Role {
+        ADMIN, USER
+    }
+}
